407a98dcbfa3d20d271af0991ca27418a67a29bf
Stolkens <92957952+Stolkens@users.noreply.github.com>
2023-05-06 19:01:07 +0200

    Add fetch, get data from API

--- src/components/WeatherBox/WeatherBox.js
+++ src/components/WeatherBox/WeatherBox.js
@@ -7,8 +7,22 @@ const WeatherBox = props => {
 
   const handleCityChange = useCallback(city => {
     console.log('cityName', city)
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=24464b13da7514c1c9fa4b29864114bf&units=metric`)
+   .then(res => res.json())
+   .then(data => {
+     const weatherData = {
+      city: data.name,
+      temperature: data.main.temp,
+      icon: data.weather[0].icon,
+      description: data.weather[0].description,
+
+    }
+    console.log(weatherData.city, weatherData.temperature, weatherData.icon, weatherData.description)
+   });
+
 
   }, []);
+  
 
   return (
     <section>
